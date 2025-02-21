827df9dd37841f3a8fd9e52be5f2053d643d4c83
Siddhi <121544611+siddhi3srivastava@users.noreply.github.com>
2025-02-21 14:58:18 +0530

    Create GCD_of_Strings(1071).cpp

---
+++ GCD_of_Strings(1071).cpp
@@ -0,0 +1,19 @@
+// GCD of two strings in C++
+
+class Solution{
+  public:
+    string GCD(string str1, string str2){
+      if(str1.length()<str2.length()){
+        return GCD(str2, str1);
+      }
+      else if(!(str1.rfind(str2,0)==0)){
+        return "";
+      }
+      else if(str2.empty()){
+        return str1;
+      }
+      else{
+        return GCD(str1.substr(str2.length()), str2);
+      }      
+   }
+};
