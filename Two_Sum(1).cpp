ffee68ccd372c6b9fa5cc21706f02f88a21eaedf
Siddhi <121544611+siddhi3srivastava@users.noreply.github.com>
2025-02-25 21:11:04 +0530

    Update Two_Sum(1).cpp

--- Two_Sum(1).cpp
+++ Two_Sum(1).cpp
@@ -1,5 +1,17 @@
+
 // Two Sum solution in c++
 
-
-
 class Solution{
+public:
+    vector<int> twoSum(vector<int>& nums, int target){
+      int len=nums.size();
+      for(int i=0; i<len; i++){
+        for(int j=i+1; j<len; j++){
+          if(nums[i]+nums[j]==target){
+            return {i,j};
+          }
+        }
+      }
+  return {};
+    }
+}
