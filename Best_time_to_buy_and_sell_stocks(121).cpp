ba57512a900f60cb9bcc91f81983a4d5b4cf8691
Siddhi <121544611+siddhi3srivastava@users.noreply.github.com>
2025-02-25 22:59:24 +0530

    Update Best_time_to_buy_and_sell_stocks(121).cpp

--- Best_time_to_buy_and_sell_stocks(121).cpp
+++ Best_time_to_buy_and_sell_stocks(121).cpp
@@ -1,1 +1,24 @@
+//C++ solution to the leetcode question 121
 
+//Using else block which reduces runtime
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int len=prices.size();
+        int minimum=INT_MAX;
+        int profit=0;
+        for(int i=0; i<len; i++){
+            if(prices[i]<minimum){
+                minimum=prices[i];
+            }
+            else{ 
+            profit=max(profit, prices[i]-minimum);
+            }
+            
+        }
+        return profit;
+
+    }
+};
