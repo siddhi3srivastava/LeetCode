9c47c4b301d46132c1f26d5bb647a6f685909c8d
Siddhi <121544611+siddhi3srivastava@users.noreply.github.com>
2025-03-05 08:17:10 +0530

    Update Two_Sum_HashMap(1).java

--- Two_Sum_HashMap(1).java
+++ Two_Sum_HashMap(1).java
@@ -1,1 +1,14 @@
-
+class Solution{
+  public int[] twoSum(int[]nums, int target){
+    Map<Integer, Integer> map=new HashMap<>();
+    int len=nums.length;
+    for(int i=0; i<len; i++){
+      int need=target-nums[i];
+      if(map.containsKey(need)){
+        return new int[]{map.get(need), i};
+      }
+      map.put(nums[i],i);
+    }
+    return new int[]{};
+  }
+}
