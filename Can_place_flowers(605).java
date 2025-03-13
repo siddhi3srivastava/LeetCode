1471bd852f9137594d7c5e1b66de7318a5ed4acf
Siddhi <121544611+siddhi3srivastava@users.noreply.github.com>
2025-03-13 13:11:18 +0530

    Create Can_place_flowers(605).java

---
+++ Can_place_flowers(605).java
@@ -0,0 +1,19 @@
+class Solution{
+  public booleancanPlaceFlowers(int[] flowerbed, int n){
+    if (n==0){
+      return true;
+    }
+    for(int i=0; i<flowerbed.length; i++){
+      if(flowerbed[i]==0 && (i==0 || flowerbed[i-1]==0) && (i==flowerbed.length-1 || flowerbead[i+1]==0)){
+        flowerbed[i]=1;
+        n--;
+        if(n==0){
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+}
+
+    
