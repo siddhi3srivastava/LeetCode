a5c358b43fc8138620178be5f386054524a1e4e1
Siddhi <121544611+siddhi3srivastava@users.noreply.github.com>
2025-04-11 13:58:13 +0530

    Create Minimum_Pair_Removal_to_Sort_Array_1(3507).java

---
+++ Minimum_Pair_Removal_to_Sort_Array_1(3507).java
@@ -0,0 +1,42 @@
+/*Given an array nums, you can perform the following operation any number of times:
+
+Select the adjacent pair with the minimum sum in nums. If multiple such pairs exist, choose the leftmost one.
+Replace the pair with their sum.
+Return the minimum number of operations needed to make the array non-decreasing.
+
+An array is said to be non-decreasing if each element is greater than or equal to its previous element (if it exists).*/
+
+//time-complexity: O(n^2)
+//space-complexity: O(1)
+
+class Solution {
+    public int minimumPairRemoval(int[] nums) {
+        int n=nums.length;
+        int count=0;
+           
+        while (n>1){
+            boolean check=true;
+            int max=Integer.MAX_VALUE;
+            int index=-1;
+            for(int i=0; i<n-1; i++){
+                if(nums[i]>nums[i+1]){
+                    check=false;
+                }
+                if(nums[i]+nums[i+1]<max){
+                    max=nums[i]+nums[i+1];
+                    index=i;
+                }
+            }
+            if(check){
+                break;
+            }
+            nums[index]=max;
+            for(int i=index+1; i<n-1; i++){
+                nums[i]=nums[i+1];
+            }
+            n--;
+            count++;
+        }
+        return count;
+    }
+}
